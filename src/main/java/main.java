d8c533eeae78e1c431c3e6d6aa4370a297b5e0eb
Jiahao Fu <jfu@bsu.edu>
2020-02-26 12:12:06 -0500

    create some comments

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,10 +9,13 @@ public class main {
 
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            //Request a string from the user
             System.out.println("Which Wikipedia page do you want to see the most recent changes to?");
             String PageKeyword = br.readLine();
+            //Pass it to the responsible classes
             String url=utils.urlToJsonString(PageKeyword);
             List<Editor> list=utils.parseJsonToList(url);
+            //Print it to the screen in reverse-chronological order
             for (int i=list.size()-1; i>=0; i--){
                 System.out.println("username: "+list.get(i).getUser()+
                         "   timestamp: "+list.get(i).getTimestamp());
